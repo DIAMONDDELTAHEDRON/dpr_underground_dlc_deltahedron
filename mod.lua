f8050c2079239a86e59fda63428bf83ed704297f
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2024-12-28 21:40:55 -0700

    Port over MGR from DLC Yellow
    (I hope it works lol)

--- mod.lua
+++ mod.lua
@@ -1,3 +1,12 @@
 function Mod:init()
     print("Loaded "..self.info.name.."!")
 end
+
+function Mod:load(data, new_file)
+	if new_file then
+		Game.money = Kristal.getLibConfig("magical-glass", "debug") and 1000 or 0
+		Game.lw_money = Kristal.getLibConfig("magical-glass", "debug") and 1000 or 0
+	end
+    MagicalGlassLib:setLightBattleShakingText(true)
+    MagicalGlassLib:setCellCallsRearrangement(true)
+end
